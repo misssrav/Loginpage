0491a2ecd12f3a93870dbc82390161ac9a46cb22
misssrav <s.naragam@csu.fullerton.edu>
2024-09-13 16:36:31 -0700

    updated changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup,FormsModule,ReactiveFormsModule,Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -12,10 +14,40 @@ export class LoginComponent {
   type: string = "password"
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder){}
+  ngOnInit():void{
+    this.loginForm=this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
   hideShowPass(){
     this.isText= !this.isText;
     this.isText? this.eyeIcon= 'fa-eye': this.eyeIcon= "fa-eye-slash";
     this.isText? this.type= "text": this.type="password"
 
+  }
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value)
+    }
+    else{
+
+      this.validateAllFormFields(this.loginForm);
+      alert("Your form is invalid")
+    }
+  }
+
+  private validateAllFormFields(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field =>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }else if (control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
+
+    })
   }
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,21 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup,FormsModule,ReactiveFormsModule,Validators } from '@angular/forms';
+
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule,CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  
   type: string = "password"
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
+  signupForm!: FormGroup;
+  constructor(private fb: FormBuilder){}
+  ngOnInit():void{
+    this.signupForm=this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required],
+      email:['',Validators.required],
+      firstname:['',Validators.required],
+      lastname:['',Validators.required]
+    })
+  }
   hideShowPass(){
     this.isText= !this.isText;
     this.isText? this.eyeIcon= 'fa-eye': this.eyeIcon= "fa-eye-slash";
     this.isText? this.type= "text": this.type="password"
 
+  }
+  onSubmit(){
+    if(this.signupForm.valid){
+      console.log(this.signupForm.value)
+    }
+    else{
+
+      this.validateAllFormFields(this.signupForm);
+      alert("Your form is invalid")
+    }
+  }
+
+  private validateAllFormFields(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field =>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }else if (control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
+
+    })
   }
 }
